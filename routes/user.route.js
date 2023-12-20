291fab648613924f3d7bd0361b0760f28df37e98
ankitgreenusys <ankit@digitalnightowl.in>
2023-12-20 17:52:24 +0530

    updates

--- routes/user.route.js
+++ routes/user.route.js
@@ -5,8 +5,8 @@ const auth = require("../middlewares/user.middleware");
 const router = Express.Router();
 
 router.post("/signin", userController.createUser);
-router.post("/verify", userController.verifyOTP);
-router.get("/resendotp/:id", userController.resendOtp);
+// router.post("/verify", userController.verifyOTP);
+// router.get("/resendotp/:id", userController.resendOtp);
 router.post("/refreshtoken", userController.refreshAccessTokenController);
 
 module.exports = router;
