31126bf0698fa961f94bd34f18271d1fadc8251c
ankitgreenusys <134594970+ankitgreenusys@users.noreply.github.com>
2023-12-23 00:14:58 +0530

    UU

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -252,7 +252,7 @@ routes.getPackage = async (req, res) => {
 
     const isBuied = user.package.find((item) => item._id === id);
 
-    return res.status(200).json({ msg: "success", dta: package, user });
+    return res.status(200).json({ msg: "success", dta: package, isBuied });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: "internal server error" });
