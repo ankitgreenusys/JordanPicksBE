59bd9b9a30d5a407a4a1c3c77667ff68e35d3fda
ankitgreenusys <ankit@digitalnightowl.in>
2024-03-20 11:21:16 +0530

    lost

--- utils/PassportSetup/GoogleAuth.js
+++ utils/PassportSetup/GoogleAuth.js
@@ -4,11 +4,8 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 passport.use(
   new GoogleStrategy(
     {
-      // clientID: process.env.GOOGLE_CLIENT_ID,
-      // clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      clientID:
-        "19246310025-40us8ovb0idsqrifh19hvbacvm2mms10.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: "/user/auth/google/callback",
       scope: ["profile", "email"],
     },
